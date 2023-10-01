507702ef8a916ba7875872e09904736fc57c76de
songkai <songkai@kuaishou.com>
2023-10-02 02:16:59 +0800

    [add] add part3

--- utils.py
+++ utils.py
@@ -5,6 +5,38 @@ import logging
 from threading import Timer
 from inspect import signature
 import time
+import numpy
+import cv2
+
+def get_homography_martix(points_1, points_2):
+    A: numpy.ndarray = numpy.zeros(shape=( len(points_1) * 2 ,  9), dtype=int)
+    for i in range(len(points_1)):
+        x1, y1 = points_1[i]
+        x2, y2 = points_2[i]
+        # -x1 * h11 - y1 * h12 - h13 + x2 = 0
+        # -x1 * h21 - y1 * h22 - h23 + y2 = 0
+        idx = i + i
+        A[idx, 0] = -x1
+        A[idx, 1] = -y1
+        A[idx, 2] = -1
+        A[idx, 3:6] = 0
+        A[idx, 6] = x1 * x2
+        A[idx, 7] = y1 * x2
+        A[idx, 8] = x2
+        idx += 1
+        A[idx, 0:3] = 0
+        A[idx, 3] = -x1
+        A[idx, 4] = -y1
+        A[idx, 5] = -1
+        A[idx, 6] = x1 * y2
+        A[idx, 7] = y1 * y2
+        A[idx, 8] = y2
+
+    _, _, Vt = numpy.linalg.svd(A)
+
+    H = Vt[-1].reshape(3, 3)
+    H /= H[2, 2]
+    return H
 
 def create_folder(folder_name: typing.AnyStr):
     if os.path.exists(folder_name):
@@ -54,6 +86,16 @@ def throttle(s, keep=60):
         return wrapped
 
     return decorate
+
+
+@throttle(0.5)
+def select_points(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
+        point_list = param.get("point_list")
+        arr_len = param.get("len", 4)
+        if len(point_list) < arr_len:
+            cv2.circle(param.get("image"), (x, y), 3, (255, 0, 0), 2)
+            point_list.append((x,y))
 
 
 def debounce(wait: int):
